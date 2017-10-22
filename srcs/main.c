11c738cd170465f813d5044b48c9f9431b4474cc
Akasha <nmougino@student.42.fr>
2017-10-22 21:04:19 +0200

    Ajout d'une fonction d'ajout simple dans l'historique pour verification

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/16 12:34:57 by nmougino          #+#    #+#             */
-/*   Updated: 2017/10/22 19:24:30 by nmougino         ###   ########.fr       */
+/*   Updated: 2017/10/22 21:03:24 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,17 @@ int			exec_mother(t_cmdl *cmdl)
 	return (1);
 }
 
+static void	to_be_fair(void)
+{
+	history_add("base64 /dev/urandom | head -c 1000 | grep 42 | wc -l | se\
+d -e 's/1/Yes/g' -e 's/0/No/g'");
+	history_add("mkdir test ; cd test ; ls -a ; ls | cat | wc -c > fifi ; c\
+at fifi");
+	history_add("cd /tmp; sort << EOF | cat -e > sorted_poem ; sed -e 's/Roses\
+/Turnips/' < sorted_poem > better_poem; cd -; echo \"I prefer turnips any\
+way\" >> /tmp/better_poem; cat /tmp/better_poem");
+}
+
 int			main(int ac, char **av, char **env)
 {
 	(void)ac;
@@ -99,11 +110,7 @@ int			main(int ac, char **av, char **env)
 	g_meta.cmdl.cmdl = NULL;
 	g_meta.cmdl.pos = 0;
 	metainit(env);
-
-	history_add("base64 /dev/urandom | head -c 1000 | grep 42 | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g'");
-	history_add("mkdir test ; cd test ; ls -a ; ls | cat | wc -c > fifi ; cat fifi");
-	history_add("cd /tmp; sort << EOF | cat -e > sorted_poem ; sed -e 's/Roses/Turnips/' < sorted_poem > better_poem; cd -; echo \"I prefer turnips anyway\" >> /tmp/better_poem; cat /tmp/better_poem");
-
+	to_be_fair();
 	while (true)
 	{
 		if (!get_cmdl(&(g_meta.cmdl)))
